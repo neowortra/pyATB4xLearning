32390824a65e350753932089e4fa44c5ca5c11b8
neowo <neowortra@gmail.com>
2024-08-14 23:09:05 +0530

    lab003

--- src/ex_08082024/lab003.py
+++ src/ex_08082024/lab003.py
@@ -1,2 +1,18 @@
 # python program 3
 print("hello world")
+print("hello", "world", 1234, True)
+print("hello", "world", 1234, True, sep="-")
+print("hello", "world", 1234, True, sep="@")
+max(121, 34, 234)
+print(max(121, 34, 234))
+print(max(121, 34, 234), "manju", 12345, False)
+age = 33
+print(age)
+age = "manju"
+print(age)
+sex = "female"
+print(sex)
+a667 = "munjju"
+print(a667)
+_age="mm"
+print(_age)
